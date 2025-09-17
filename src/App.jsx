682bf6e4c68d31217071a3a6c230b5d7e419d085
unknown <b.wilki0109@gmail.com>
2025-09-17 08:13:45 +0200

    create timerChallange component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
+import TimerChallange from './components/TimerChallange.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+        <TimerChallange targetTime={1} title={'Easy'} />
+        <TimerChallange targetTime={5} title={'Not easy'} />
+        <TimerChallange targetTime={10} title={'Getting tough'} />
+        <TimerChallange targetTime={15} title={'pros only'} />
+      </div>
     </>
   );
 }
